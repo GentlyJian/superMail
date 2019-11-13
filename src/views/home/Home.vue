34802d878b6eb7df34327f8d9f2a2058b31590e4
zzj <1183048003@qq.com>
2019-11-13 16:50:45 +0800

    创建头部导航栏1

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>主页</h1>
+  <nav-bar class="home-nav">
+    <template  #navCenter>双十一购物狂欢</template>
+  </nav-bar>
 </template>
 <script>
+import NavBar from 'components/common/navBar/NavBar';
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    NavBar
+  }
 }
 </script>
 <style scoped>
-
+  .home-nav {
+    color: #fff;
+    background-color: var(--color-tint);
+  }
 </style>
