1cfa0c7705b7556a3abafcc49434daf1812400aa
zzj <1183048003@qq.com>
2019-11-13 14:50:23 +0800

    初始化样式&设置别名

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+modules.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'views': '@/views',
+        'network': '@/network',
+      }
+    }
+  }
+}
